acb7052b59158b74264cc21efc0fbe377701e5c6
Amit Vishwakarma <amitamitamit111@gmail.com>
2021-10-10 13:46:24 +0530

    Initial commit

---
+++ order-service/src/test/java/com/jarvis/orderservice/api/OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarvis.orderservice.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
